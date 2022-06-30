55ee0a254de796e70a351a02a2fb9a56325d313c
vldslt <vldslt666@gmail.com>
2022-06-30 23:35:26 +0300

    теперь для перехода используется кнопка

--- app/src/main/java/com/example/androidstart/MainActivity.java
+++ app/src/main/java/com/example/androidstart/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidstart;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.info_maket);
+        setContentView(R.layout.activity_main);
+
+        Button detailButton = findViewById(R.id.toggleButton);
+
+
+        detailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, MainActivity2.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
